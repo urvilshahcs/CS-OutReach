6c0a8b39403a4d9582b9a7a8a853bd8c342f6c9e
Urvil Shah <urvilshahcs@gmail.com>
2017-07-17 10:09:30 -0700

    Additional Files

---
+++ CsOutreach/CSOutreach/Role.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CSOutreach
+{
+    public enum Role
+    {
+        STUDENT,
+        ADMIN,
+        INSTRUCTOR,
+        ANONYMOUS
+    }
+}
