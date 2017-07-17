6148584422c6bd94e9659c7b60e3eeb840d9a948
Urvil Shah <urvilshahcs@gmail.com>
2017-07-17 10:10:45 -0700

    UI

---
+++ CsOutreach/CSOutreach/UI/MenuElement.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CSOutreach.UI
+{
+    //This Comment....
+    public class MenuElement
+    {
+        public string MenuName
+        {
+            get;
+            set;
+        }
+        public string MenuURL
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ CsOutreach/CSOutreach/UI/MenuRender.cs
@@ -0,0 +1,52 @@
+﻿using DataOperations.DBEngine;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace CSOutreach.UI
+{
+    public class MenuRender
+    {
+        private static string DynamicMenuString;
+        public static string DynamicMenu
+        {
+            get
+            {
+                return DynamicMenuString;
+            }
+        }
+        public static List<MenuElement> DynamicMenuItems
+        {
+            get
+            {
+                List<MenuElement> MenuItems = new List<MenuElement>();
+                DBManager DbManager = new DBManager();
+                DataSet MenuRaw = DbManager.ExecuteReadSP("ListAllMenuItems");
+                foreach(DataRow Row in MenuRaw.Tables[0].Rows)
+                {
+                    MenuElement MnuElement = new MenuElement();
+                    MnuElement.MenuName = Row[1].ToString();
+                    MnuElement.MenuURL = Row[2].ToString();
+                    MenuItems.Add(MnuElement);
+                }
+                return MenuItems;
+            }
+        }
+
+        public static void RenderDynamicMenu()
+        {
+
+            DynamicMenuString = "<li class=\"dropdown\">" +
+                   "<a href=\"#\" class=\"dropdown-toggle\" data-toggle=\"dropdown\">Activities<span class=\"caret\"></span></a>" +
+                   "<ul class=\"dropdown-menu\" role=\"menu\">";
+            foreach(MenuElement Element in DynamicMenuItems)
+            {
+                DynamicMenuString += "<li><a href=\"" + Element.MenuURL + "\">" + Element.MenuName + "</a></li>";
+            }
+            DynamicMenuString += "</ul></li>";
+
+        }
+    }
+}
